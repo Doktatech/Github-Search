ac3601bd012e890d39105504d5fa74f14799c1a1
Doktatech <Doktatech2@gmail.com>
2019-05-07 07:44:05 +0300

    Import and link service for user fetching

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { error } from 'util';
+import{User} from '../user';
+import {UserService} from '../user.service';
 
 @Component({
   selector: 'app-search-user',
   templateUrl: './search-user.component.html',
-  styleUrls: ['./search-user.component.css']
+  styleUrls: ['./search-user.component.css'],
+  providers:[UserService]
 })
 export class SearchUserComponent implements OnInit {
-  GithubUserName:string=""
-  
-
-  constructor() { }
-
-  ngOnInit() {
+  githubUserName:string=""
+  githubUser:User[]=[]
+  loading:boolean=false;
+  errorAlert; 
+  constructor(private userService:UserService) {   
+}
+public fetchUser(){
+    this.loading=true;
+    this.errorAlert="";
+      this.userService.fetchUser(this.githubUserName) 
+      .subscribe((response)=> {this.githubUser=response;},
+      (error)=>{this.errorAlert=error; this.loading=false;},
+      ()=> {this.loading=false;})
   }
-
+   ngOnInit() {
+  }
 }
