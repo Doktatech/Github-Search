09a1917ae3ddd8d77e8dcdb13e91d088e1692db1
Doktatech <Doktatech2@gmail.com>
2019-05-07 07:44:29 +0300

    Import and link service for fetching repos

---
+++ src/app/github-repos/github-repos.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import{Repository} from '../repository';
+import {RepositoryService} from '../repository.service';
+
+@Component({
+  selector: 'app-github-repos',
+  templateUrl: './github-repos.component.html',
+  styleUrls: ['./github-repos.component.css'],
+  providers:[RepositoryService]
+})
+export class GithubReposComponent implements OnInit {
+  githubUserName:string=""
+  Repos:Repository[]=[]
+  loading:boolean=false
+  errorAlert
+constructor(private repositoryService:RepositoryService){
+}
+public fetchUser(){
+  this.loading=true;
+  this.errorAlert="";
+        this.repositoryService.fetchUser(this.githubUserName)
+            .subscribe((response) => {this.Repos=response;},
+            (error) => {this.errorAlert=error; this.loading=false; },
+            () => {this.loading=false;})
+    }
+
+  ngOnInit() {
+  }
+
+}
