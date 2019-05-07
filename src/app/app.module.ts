ecc430910736bda1a6b4191f38333463c7149dd2
Doktatech <Doktatech2@gmail.com>
2019-05-07 07:49:44 +0300

    Import the modules , declarations and providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchUserComponent } from './search-user/search-user.component';
-
+import { GithubReposComponent } from './github-repos/github-repos.component';
+const routes:Routes=[
+  
+  {path:"search-user",component:SearchUserComponent},
+  {path:"github-repos",component:GithubReposComponent}
+]
+ 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchUserComponent
+    SearchUserComponent,
+    GithubReposComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
